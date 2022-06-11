62fbd8e1787fa8772e6acb5621315e3ef42e845f
renhualu <1472638@qq.com>
2022-06-11 16:48:43 +0800

    Update main.go

--- src/for2/main.go
+++ src/for2/main.go
@@ -1,25 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var month byte
-	fmt.Println("input a score:")
-	fmt.Scanln(&month)
 
-	switch month {
-	case 3, 4, 5:
-		fmt.Println("spring")
-	case 6, 7, 8:
-		fmt.Println("summer")
-	case 9, 10, 11:
-		fmt.Println("autumn")
-	case 12, 1, 2:
-		fmt.Println("winter")
-	default:
-		fmt.Println("Unknow")
+	for i := 0; i < 10; i++ {
+		fmt.Println("welcome")
 	}
-
 }
